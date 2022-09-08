bb2475dc9d6c3c75f7bfba96f2634175ffa515bc
ranjith1707 <109972671+ranjith1707@users.noreply.github.com>
2022-09-08 21:42:57 +0530

    Add files via upload

--- StringPatten.java
+++ StringPatten.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class StringPatten {
+public class StringPatten{
     public static void main(String[] args){
 Scanner input=new Scanner(System.in);
 String str=input.next();
